4b06b2046e99110b8ff4282bca1d43307b008ed0
FFaUniHan <handersondavistjia@yahoo.com>
2022-04-06 19:21:43 +0700

    After Tutorial Commit

--- After Tutorial/Assets/ExpressionControl.cs
+++ After Tutorial/Assets/ExpressionControl.cs
@@ -9,20 +9,24 @@ public class ExpressionControl : MonoBehaviour
     public void SetExpression_Default()
     {
         Debug.Log("Animation Default");
+        anim.SetTrigger("Default");
     }
 
     public void SetExpression_Joy()
     {
         Debug.Log("Animation Joy");
+        anim.SetTrigger("Joy");
     }
 
     public void SetExpression_Surprised()
     {
         Debug.Log("Animation Surprised");
+        anim.SetTrigger("Surprised");
     }
 
     public void SetExpression_Sus()
     {
         Debug.Log("Animation Sus");
+        anim.SetTrigger("Sus");
     }
 }
